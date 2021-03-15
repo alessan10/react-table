78c2193e073ddab8bbb2c151821a36d77cd23cb5
alessandromauro.m@outlook.com <alex.mauro@live.it>
2021-03-15 12:42:41 +0100

    basic table app works

---
+++ react_table_demo/src/components/Basictable.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import { useTable } from 'react-table'
+import MOCK_DATA from '../mock/MOCK_DATA.json'
+import { COLUMNS } from './columns';
+
+import { useMemo } from 'react';
+
+import './table.css'
+
+//da qui
+export const BasicTable = () =>{
+
+    //prima della chiamata di useTable 
+    const columns = useMemo (() => COLUMNS, []) //arrow function che ritorna le colonne e un array vuoto
+    const data = useMemo (() => MOCK_DATA, []) // stessa cosa, ritorna MOCK_DATA e un empty dependency array
+
+    const tableInstance = useTable({
+        /*columns: columns,
+        data: data*/
+        //questo può essere semplificato grazie alla sintassi es6 in:
+        
+        columns,
+        data
+    })
+    
+    //use table ritorna una table instance che memorizziamo in una costante
+
+    const { 
+        getTableProps, 
+        getTableBodyProps, 
+        headerGroups, 
+        rows, 
+        prepareRow 
+    } = tableInstance    
+
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {
+                    headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}> 
+                        {
+                            headerGroup.headers.map((column) => (
+                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                            ))
+                        }
+                            
+                        </tr>
+                    ))
+                }
+                
+            </thead>
+
+            <tbody {...getTableBodyProps()}>
+                {
+                    rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map((cell) =>{
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                    })
+                                }
+                               
+                            </tr>
+                        )
+                    })
+                }
+                
+            </tbody>
+        </table>
+    )
+}
+//a qui
+//questo è uno snippet creato con rafc (a me non funziona)
